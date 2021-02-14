66ba025b340fbb2bbdacfec0f2dc599bdc8a2c93
overcrate <qirnilius@gmail.com>
2021-02-14 17:43:15 +0100

    Update Reachable.cpp

--- src/reachable/Reachable.cpp
+++ src/reachable/Reachable.cpp
@@ -6,7 +6,7 @@
 
 namespace ClassProject {
        
-    Reachable::Reachable(unsigned int x){
+    Reachable::Reachable(unsigned int x) : ReachableInterface(x){
     stateSize=x;
         for (int i = 0; i< stateSize; i++){
             BDD_ID var_id = createVar("s" + std::to_string(i));
