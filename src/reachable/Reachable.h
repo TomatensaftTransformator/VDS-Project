c90369af149aa5ef8c5df0f050231bbfc6660228
overcrate <qirnilius@gmail.com>
2021-02-14 17:10:18 +0100

    Update Reachable.h

--- src/reachable/Reachable.h
+++ src/reachable/Reachable.h
@@ -18,14 +18,13 @@ namespace ClassProject {
     class Reachable : public ReachableInterface {
         public:
             //using ReachableInterface::ReachableInterface; //get constructor of ReachableInterface!
-            //Reachable() = delete;
+            Reachable() = delete;
 
             /**
              * Constructor creates stateSize state bits for the user
              * @param stateSize state size
              */
-            //explicit 
-            Reachable(unsigned int x);
+            explicit Reachable(unsigned int x);
             virtual ~Reachable() = default;
 
 
